36194da92792b854e072d4889f77cfb25772e81c
ajstun <64382439+ajstun@users.noreply.github.com>
2022-03-31 14:12:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# arunjayakumar.com
+ 
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1>Hey, A</h1>
+        <p>A Fresh, New React Boiler-Plate to create a phenomenal website
+            1
+        </p>
+    </div>
+  )
+}
+
+export default App
